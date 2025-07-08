86bbcbd5df339b73267360e9777aee7fd976d8b8
Adham Mustafa <adhoomy.mustafa@gmail.com>
2025-07-07 22:37:49 -0400

    Finished delete job controller, finished basic functionality of project.

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -43,7 +43,18 @@ const updateJob = async(req, res) => {
     res.status(StatusCodes.OK).json({job})
 }
 const deleteJob = async(req, res) => {
-    res.send('delete job')
+    const {
+        user: {userId},
+        params: {id: jobId}
+    } = req
+    const job = await Job.findByIdAndDelete({
+        _id: jobId,
+        createdBy: userId,
+    })
+    if(!job) {
+        throw new NotFoundError(`No job with id ${jobId}`)
+    }
+    res.status(StatusCodes.OK).send()
 }
 
 module.exports = {
